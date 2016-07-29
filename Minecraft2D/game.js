9012088949cf5a028586268444268c63fb1ea899
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-07-29 11:19:56 +0200

    Colors and initial world, ground and grass!

--- Minecraft2D/game.js
+++ Minecraft2D/game.js
@@ -10,15 +10,20 @@ $(function() {
 
     // Buffers.
     var vertexBuffer;
+    var colorBuffer;
 
     // Initialize buffers.
+    var vPosition;
+    var vColor;
 
     // Game related stuff.
     var worldWidth = 40;
     var worldHeight = 40;
+    var worldBlocks = worldWidth * worldHeight;
     //var squareSize = canvas.clientWidth / 10;
     var worldGrid = [];
     var points = [];
+    var colors = [];
 
     var render_scale = 2 / Math.max(worldWidth, worldHeight);
 
@@ -35,21 +40,57 @@ $(function() {
         FIRE: 7
     }
 
-    // Generate world.
+    function tile_to_color(block)
+    {
+        switch(block)
+        {
+            case blocks.EMPTY:
+                return vec4(0., 0., 1., 0.4);
+            case blocks.STONE:
+                return vec4(5., 5., 5., 1.);
+            case blocks.GRASS:
+                return vec4(0., 1., 0., 1.);
+            case blocks.DIRT:
+                return vec4(0.55, 0.27, 0.07, 1.);
+            // TODO: Rest of cases
+        }
+    }
+
+    // Generate empty world.
     for (var x = 0; x < worldWidth; x++) {
         worldGrid[x] = [];
         for (var y = 0; y < worldHeight; y++) {
             worldGrid[x][y] = {
-                tile: blocks.empty,
+                tile: blocks.EMPTY,
                 pos: vec2(x + 0.5, y + 0.5)
+                //rendered: false
             }
         }
+    }
+
+    // Create ground
+    for (var x = 0; x < worldWidth; x++) {
+        for (var y = 0; y < Math.floor(worldHeight/3); y++) {
+            worldGrid[x][y].tile = blocks.DIRT;
+        }
+    }
+
+    // Create grass
+    for (var x = 0; x < worldWidth; x++) {
+        var y = Math.floor(worldHeight/3);
+        worldGrid[x][y].tile = blocks.GRASS;
+        worldGrid[x][y+1].tile = blocks.GRASS;
     }
 
     function flatten2dArray(pointsArray) {
         for (var x = 0; x < pointsArray.length; x++) {
             for (var y = 0; y < pointsArray[x].length; y++) {
-                points.push(pointsArray[x][y].pos);
+                var point = pointsArray[x][y];
+                if(point.tile != blocks.EMPTY)
+                {
+                    points.push(point.pos);
+                    colors.push(tile_to_color(point.tile));
+                }
             }
         }
     }
@@ -57,7 +98,15 @@ $(function() {
 
     function render() {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+        gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+        gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+
         gl.drawArrays(gl.POINTS, 0, points.length);
     }
     render();
@@ -82,10 +131,20 @@ $(function() {
     function initBuffers() {
         vertexBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, sizeof['vec2'] * worldBlocks, gl.STATIC_DRAW);
+
+        colorBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, sizeof['vec4'] * worldBlocks, gl.STATIC_DRAW);
+ 
 
         var vPosition = gl.getAttribLocation(program, 'vPosition');
         gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vPosition);
+
+        var vColor = gl.getAttribLocation(program, "vColor");
+        gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vColor);
 
         var vScalePos = gl.getUniformLocation(program, "vScale");
         gl.uniform1f(vScalePos, render_scale);
