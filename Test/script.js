f010ed41ac459d2e7bb3262227c783fc739ca3ab
dkchokk <christoffer@okkels.org>
2016-08-02 11:13:49 +0200

    object for easily managing program buffers

--- Test/script.js
+++ Test/script.js
@@ -16,6 +16,8 @@ var ShaderProgram = (function () {
         var vertexElement = document.getElementById(vertexShaderId);
         var fragmentElement = document.getElementById(fragmentShaderId);
 
+        var msg;
+
         if (!vertexElement || !fragmentElement)
             throw new Error('Needs id of vertex- and fragmentshader script elements.');
 
@@ -34,7 +36,7 @@ var ShaderProgram = (function () {
         // Check for errors.
         if (!this._gl.getShaderParameter(vertexShader, this._gl.COMPILE_STATUS) ||
             !this._gl.getShaderParameter(fragmentShader, this._gl.COMPILE_STATUS)) {
-            var msg = 'Shader errors:' + '\n';
+            msg = 'Shader errors:' + '\n';
             var vertexShaderLog = this._gl.getShaderInfoLog(vertexShader);
             var fragmentShaderLog = this._gl.getShaderInfoLog(fragmentShader);
             if (vertexShaderLog.length > 0)
@@ -56,10 +58,9 @@ var ShaderProgram = (function () {
 
         // Check link successful.
         if (!this._gl.getProgramParameter(this._shaderProgram, this._gl.LINK_STATUS)) {
-            var msg = 'Link error:' + '\n';
+            msg = 'Link error:' + '\n';
             msg += this._gl.getProgramInfoLog(this._shaderProgram);
             alert(msg);
-            return;
         }
     }
 
@@ -67,18 +68,46 @@ var ShaderProgram = (function () {
         this._gl.useProgram(this._shaderProgram);
     };
 
-    ShaderProgram.prototype.createAttribute = function (name) {
+    ShaderProgram.prototype.setBindings = function () {
+        this.setActive();
+        for (var i = 0; i < this._attributes.length; i++) {
+            var attribute = this._attributes[i];
+            this._gl.bindBuffer(this._gl.ARRAY_BUFFER, attribute.buffer);
+            this._gl.vertexAttribPointer(attribute.index, attribute.numComponents, this._gl.FLOAT, false, 0, 0);
+            this._gl.enableVertexAttribArray(attribute.index);
+        }
+    };
+
+    ShaderProgram.prototype.createAttribute = function (name, type) {
         var attribute = {
             name: name,
+            numComponents: undefined,
             index: undefined,
-            buffer: undefined
+            buffer: undefined,
+            type: type
         };
         attribute.buffer = this._gl.createBuffer();
         attribute.index = this._gl.getAttribLocation(this._shaderProgram, attribute.name);
+        attribute.numComponents = this.getNumComponents(attribute.type);
         this._attributes.push(attribute);
     };
 
-    ShaderProgram.prototype.setAttributeData = function (name, data) {
+    ShaderProgram.prototype.setAttributeData = function (name, sizeOrData) {
+        var attribute = this.getAttribute(name);
+        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, attribute.buffer);
+        if (typeof sizeOrData === 'number')
+            this._gl.bufferData(this._gl.ARRAY_BUFFER, this.getByteSize(attribute.type) * sizeOrData, this._gl.STATIC_DRAW);
+        else
+            this._gl.bufferData(this._gl.ARRAY_BUFFER, flatten(sizeOrData), this._gl.STATIC_DRAW);
+    };
+
+    ShaderProgram.prototype.setAttributeSubData = function (name, data, offset) {
+        var attribute = this.getAttribute(name);
+        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, attribute.buffer);
+        this._gl.bufferSubData(this._gl.ARRAY_BUFFER, offset * this.getByteSize(attribute.type), flatten(data));
+    };
+
+    ShaderProgram.prototype.getAttribute = function (name) {
         var attribute;
         for (var i = 0; i < this._attributes.length; i++) {
             if (this._attributes[i].name === name) {
@@ -88,17 +117,56 @@ var ShaderProgram = (function () {
         }
         if (!attribute)
             throw new Error('Attribute "' + name + '" not found.');
-        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, attribute.buffer);
-        this._gl.bufferData(this._gl.ARRAY_BUFFER, flatten(data), this._gl.STATIC_DRAW);
+        return attribute;
     };
 
-    ShaderProgram.prototype.setBindings = function () {
-        this.setActive();
-        for (var i = 0; i < this._attributes.length; i++) {
-            var attribute = this._attributes[i];
-            this._gl.bindBuffer(this._gl.ARRAY_BUFFER, attribute.buffer);
-            this._gl.vertexAttribPointer(attribute.index, 2, this._gl.FLOAT, false, 0, 0);
-            this._gl.enableVertexAttribArray(attribute.index);
+    ShaderProgram.prototype.getByteSize = function (typeString) {
+        switch(typeString) {
+            case 'vec2':
+                return sizeof['vec2'];
+                break;
+            case 'vec3':
+                return sizeof['vec3'];
+                break;
+            case 'vec4':
+                return sizeof['vec4'];
+                break;
+            case 'mat2':
+                return sizeof['mat2'];
+                break;
+            case 'mat3':
+                return sizeof['mat3'];
+                break;
+            case 'mat4':
+                return sizeof['mat4'];
+                break;
+            default:
+                return -1;
+        }
+    };
+
+    ShaderProgram.prototype.getNumComponents = function (typeString) {
+        switch(typeString) {
+            case 'vec2':
+                return 2;
+                break;
+            case 'vec3':
+                return 3;
+                break;
+            case 'vec4':
+                return 4;
+                break;
+            case 'mat2':
+                return 4;
+                break;
+            case 'mat3':
+                return 9;
+                break;
+            case 'mat4':
+                return 16;
+                break;
+            default:
+                return -1;
         }
     };
 
@@ -124,34 +192,37 @@ window.onload = function init() {
         points.push(a, b, c);
     }
 
+    var colors = [
+        vec4(1., 0., 0., 1.), vec4(1., 0., 0., 1.), vec4(1., 0., 0., 1.),
+        vec4(0., 1., 0., 1.), vec4(0., 1., 0., 1.), vec4(0., 1., 0., 1.),
+        vec4(0., 0., 1., 1.), vec4(0., 0., 1., 1.), vec4(0., 0., 1., 1.),
+        vec4(1., 0., 1., 1.), vec4(1., 0., 1., 1.), vec4(1., 0., 1., 1.)
+    ];
+
     triangle(vec2(.5, .5), vec2(1., .5), vec2(.75, 1.));
     triangle(vec2(.5, -.5), vec2(1., -.5), vec2(.75, -1.));
     triangle(vec2(-.5, -.5), vec2(-1., -.5), vec2(-.75, -1.));
     triangle(vec2(-.5, .5), vec2(-1., .5), vec2(-.75, 1.));
 
-    //  Configure WebGL
-
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    //  Load shaders and initialize attribute buffers
-
     shaderProgram = new ShaderProgram(gl, 'vertex-shader', 'fragment-shader');
     shaderProgram.setActive();
-    shaderProgram.createAttribute('vPosition', 2);
-    shaderProgram.setAttributeData('vPosition', flatten(points));
 
-    // Load the data into the GPU
+    // Set all data at once.
+    shaderProgram.createAttribute('vPosition', 'vec2');
+    shaderProgram.setAttributeData('vPosition', points);
 
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    // Set size on create.
+    shaderProgram.createAttribute('vColor', 'vec4');
+    shaderProgram.setAttributeData('vColor', colors.length);
 
-    // Associate out shader variables with our data buffer
-
-    // var vPosition = gl.getAttribLocation(program, "vPosition");
-    // gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    // gl.enableVertexAttribArray(vPosition);
+    // Set data for each triangle.
+    shaderProgram.setAttributeSubData('vColor', colors.slice(6, 9), 0);
+    shaderProgram.setAttributeSubData('vColor', colors.slice(3, 6), 6);
+    shaderProgram.setAttributeSubData('vColor', colors.slice(0, 3), 3);
+    shaderProgram.setAttributeSubData('vColor', colors.slice(9, 12), 9);
 
     render();
 };
